19bb7692ca08cca231d6f238b62c5ad952441651
loreb <lorebaia@gmail.com>
2023-09-25 19:23:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "EmailGenerator.h"
+
+#define N_MAILS 1000
+
+int main() {
+    std::string emails[N_MAILS];
+    EmailGenerator eg = EmailGenerator();
+    for(auto & email : emails)
+        email = eg.generateEmail();    return 0;
+}
